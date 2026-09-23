7e144b422d8205eed86400dd19d8ef549e1a91b5
ماجد حسين صالح قايد الحدي <23160077@su.edu.ye>
2026-09-23 04:49:22 +0300

    feat: complete authentication flow and update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,9 @@
 import { Link } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
 export default function Navbar() {
+  const { user, logout } = useContext(AuthContext);
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
@@ -15,14 +19,23 @@ export default function Navbar() {
           </Link>
 
           <div className="navbar-auth">
-            <div className="navbar-auth-links">
-              <Link to="/auth" className="btn btn-secondary">
-                Login
-              </Link>
-              <Link to="/auth" className="btn btn-primary">
-                Signup
-              </Link>
-            </div>
+            {!user ? (
+              <div className="navbar-auth-links">
+                <Link to="/auth" className="btn btn-secondary">
+                  Login
+                </Link>
+                <Link to="/auth" className="btn btn-primary">
+                  Signup
+                </Link>
+              </div>
+            ) : (
+              <div className="navbar-user">
+                <span>Hello, {user.email}</span>
+                <button className="btn btn-secondary" onClick={logout}>
+                  Logout
+                </button>
+              </div>
+            )}
           </div>
         </div>
       </div>
